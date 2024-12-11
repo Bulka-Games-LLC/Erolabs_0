59d302edc8629c5435d91c46c78d25069f4ce97e
Aleksandr Polianskii <drsage.pro@gmail.com>
2024-12-12 00:12:35 +0300

    try4

--- TemplateData/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('fetch', (event) => {
-  const request = event.request;
-  
-  // Access the request headers
-  console.log('Request Headers:', request.headers);
-
-  // You can even modify or respond to the request here if needed
-  event.respondWith(fetch(request));
-});
